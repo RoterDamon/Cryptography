32a5f610e3c847a220ddff084450d2bdf33ee234
RoterDamon <80481177+RoterDamon@users.noreply.github.com>
2022-04-05 10:37:48 +0300

    crypta2.0

---
+++ Crypta/Crypta/ThirdInterface2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    interface ThirdInterface2
+    {
+        byte[] EncryptFunc(byte[] block, byte[] roundKey);
+    }
+}
